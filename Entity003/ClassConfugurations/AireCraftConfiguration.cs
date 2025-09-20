170529c66033ea462b1d436381599b9f27091a74
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-09-20 04:55:25 +0300

    CeateDatabaseRelations

---
+++ Entity003/ClassConfugurations/AireCraftConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Entity003.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity003.ClassConfugurations
+{
+    internal class AireCraftConfiguration : IEntityTypeConfiguration<AireCraft>
+    {
+        public void Configure(EntityTypeBuilder<AireCraft> builder)
+        {
+            builder.OwnsOne(a => a.crewDetails)
+                .WithOwner();
+
+
+            
+        }
+    }
+}
---
+++ Entity003/Models/AireCraft.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity003.Models
+{
+    public class AireCraft
+    {
+        public int Id { get; set; }
+
+        public string Model { get; set; } = null!;
+        public int  Capcity { get; set; }
+
+
+        // add crew in Airecraft Table 
+        public Crew crewDetails { get; set; } =null!;
+
+
+
+        //aire craft owend by one aire lines
+        public int AireLineOwnerId;
+        public AireLine AireLine { get; set; } = null!;
+
+
+       public ICollection<AireCraftRoute> AireCraftAssign { get; set; }= new List<AireCraftRoute>();  
+      
+    }
+}
